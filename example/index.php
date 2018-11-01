f5299d11c4ecb78185caa7e3ae2fff7ba6e308fd
Craig Francis <craig@craigfrancis.co.uk>
2018-11-01 14:16:58 +0000

    Update index.php to match how I'm using it now (ish)

--- example/index.php
+++ example/index.php
@@ -40,33 +40,53 @@
 	$hmrc_gateway->sender_set($config_sender_name, $config_sender_pass, $config_sender_email);
 
 //--------------------------------------------------
+// Pending requests
+
+	$requests = $hmrc_gateway->request_list('HMRC-PAYE-RTI-FPS');
+	if (count($requests) > 0) {
+
+		// foreach ($requests as $request) {
+		// 	$hmrc_gateway->request_delete($request);
+		// }
+
+		// foreach ($requests as $request) {
+		// 	print_r($request);
+		// 	$request = $hmrc_gateway->request_poll($request);
+		// 	print_r($request);
+		// 	exit();
+		// }
+
+		print_r($requests);
+
+		exit('Incomplete requests found.');
+
+	}
+
+//--------------------------------------------------
 // Example employee
 
-	$example_employee = array(
+	$example_employee = [
 
 			'national_insurance_number' => 'AB164231A',
 
-			'name' => array(
-					'title' => 'Mr',
+			'name' => [
+					'title'    => 'Mr',
 					'forename' => 'John',
-					'surname' => 'Smith',
-				),
+					'surname'  => 'Smith',
+				],
 
-			'address' => array(
-					'lines' => array(
+			'address' => [
+					'lines' => [
 							'1 Street',
 							'Gosforth',
 							'Town',
-						),
+						],
 					'postcode' => 'AA11 1AA',
-				),
+				],
 
 			'birth_date' => '2000-01-01', // Date of birth
 			'gender'     => 'M',          // Current gender
 			'pay_id'     => '123-A02',    // Payroll ID
-
-			'to_date_taxable' => '8000.00', // Taxable pay to date in this employment including taxable benefits undertaken through payroll
-			'to_date_tax'     => '756.76',  // Total tax to date in this employment including this submission
 
 			'payment_frequency' => 'M1',         // Pay frequency (e.g. W1 = Weekly, W2 = Fortnightly, W4 = 4 Weekly, M1 = Calendar Monthly, etc)
 			'payment_date'      => '2013-05-31', // Payment date
@@ -79,32 +99,34 @@
 
 			'ni_letter' => 'A', // National Insurance Category letter in pay period
 
+			'to_date_taxable' => '8000.00', // Taxable pay to date in this employment including taxable benefits undertaken through payroll
+			'to_date_tax'     => '756.76',  // Total tax to date in this employment including this submission
+
 			'ni_gross_nics_pd'  => '4000.00', // Gross earnings for NICs in this period.
 			'ni_gross_nics_ytd' => '8000.00', // Gross earnings for NICs year to date.
 
+			'ni_total_lel_ytd' => '928.00',  // Value of Earnings at Lower Earnings Limit Year to Date.
+			'ni_total_pt_ytd'  => '5406.00', // Value of Earnings above Lower Earnings Limit to Primary Threshold Year to Date.
+			'ni_total_uel_ytd' => '0.00',    // Value of Earnings from Upper Accrual Point up to Upper Earnings Limit Year to Date.
+
 			'ni_total_nic_pd'  => '465.88',  // Total of employer NI Contributions in this period.
 			'ni_total_nic_ytd' => '931.76',  // Total of employer NI contributions year to date.
-
-			'ni_total_lel_ytd' => '928.00',  // Value of Earnings at Lower Earnings Limit Year to Date.
-			'ni_total_pt_ytd'  => '340.00',  // Value of Earnings above Lower Earnings Limit to Primary Threshold Year to Date.
-			'ni_total_uap_ytd' => '5406.00', // Value of Earnings from the Primary Threshold to Upper Accrual Point Year to Date.
-			'ni_total_uel_ytd' => '406.00',  // Value of Earnings from Upper Accrual Point up to Upper Earnings Limit Year to Date.
 
 			'ni_total_contribution_pd'  => '0.00', // Employees contributions due on all earnings in this pay period.
 			'ni_total_contribution_ytd' => '0.00', // Employees contributions due on all earnings year to date.
 
 				// Calculations: http://www.hmrc.gov.uk/rti/developerfaqs.htm
 
-		);
+		];
 
 //--------------------------------------------------
 // Final submission information
 
-	$final = false;
+	$final = false; // e.g. ($payment_month == 12)
 
 	if ($final) {
 
-		$final = array(
+		$final = [
 
 				'free_of_tax_payments'         => false, // true if you made any payments to any employees while they were employed by you where you paid their tax on their behalf.
 				'expenses_and_benefits'        => false, // true if anyone, other than you, paid expenses or provided benefits to any of your employees during the year as a result of the employee working for you, and while they were employed by you.
@@ -121,12 +143,55 @@
 					// - the limited company, limited liability partnership or general partnership's (the service company)
 					//   business consists wholly or mainly of providing the services of individuals to clients.
 
-			);
+			];
 
 	}
 
 //--------------------------------------------------
-// Delete requests
+// Create request
+
+	// $hmrc_rti = new hmrc_rti_eas(); // Employer Alignment Submission
+
+	$hmrc_rti = new hmrc_rti_fps(); // Full Payment Submission
+
+	$hmrc_rti->details_set([
+			'year' => 2013,
+			'final' => $final,
+			'tax_office_number' => $config_tax_office_number,
+			'tax_office_reference' => $config_tax_office_reference,
+			'accounts_office_reference' => $config_accounts_office_reference,
+			'corporation_tax_reference' => $config_corporation_tax_reference,
+		]);
+
+	$hmrc_rti->employee_add($example_employee);
+
+	// $xml = $hmrc_rti->request_body_get_xml();
+
+//--------------------------------------------------
+// Send and poll for response
+
+	$request = $hmrc_gateway->request_submit($hmrc_rti);
+
+	// print_r($request);
+
+	$k = 0;
+
+	while ($request['status'] === NULL && $k++ < 5) {
+
+		$request = $hmrc_gateway->request_poll($request);
+
+		// print_r($request);
+
+	}
+
+	if ($request['status'] === NULL) {
+		exit('Stopped waiting for a response after several attempts.');
+	}
+
+//--------------------------------------------------
+// Delete request
+
+	$hmrc_gateway->request_delete($request);
 
 	// $requests = $hmrc_gateway->request_list('HMRC-PAYE-RTI-EAS');
 	// print_r($requests);
@@ -135,64 +200,10 @@
 	// }
 	// exit('Deleted');
 
-	// $hmrc_gateway->request_delete(array(
+	// $hmrc_gateway->request_delete([
 	// 		'class' => 'HMRC-PAYE-RTI-EAS',
 	// 		'correlation' => 'DF64ED198BEB43178A0C6A3CCE7D389C',
-	// 	));
+	// 	]);
 
-//--------------------------------------------------
-// Pending requests
-
-	// $requests = $hmrc_gateway->request_list('HMRC-PAYE-RTI-EAS');
-	// print_r($requests);
-	// exit();
-
-	// $requests = $hmrc_gateway->request_list('HMRC-PAYE-RTI-FPS');
-	// foreach ($requests as $request) {
-	//
-	// 	print_r($request);
-	// 	$request = $hmrc_gateway->request_poll($request);
-	// 	print_r($request);
-	// 	exit();
-	//
-	// }
-
-//--------------------------------------------------
-// Create request
-
-	$hmrc_rti = new hmrc_rti_eas();
-	// $hmrc_rti = new hmrc_rti_fps();
-
-	$hmrc_rti->details_set(array(
-			'year' => 2013,
-			'final' => $final,
-			'tax_office_number' => $config_tax_office_number,
-			'tax_office_reference' => $config_tax_office_reference,
-			'accounts_office_reference' => $config_accounts_office_reference,
-			'corporation_tax_reference' => $config_corporation_tax_reference,
-		));
-
-	$hmrc_rti->employee_add($example_employee);
-
-//--------------------------------------------------
-// Send and poll for response
-
-	$request = $hmrc_gateway->request_submit($hmrc_rti);
-
-	print_r($request);
-
-	$k = 0;
-
-	while ($request['status'] === NULL && $k++ < 5) {
-
-		$request = $hmrc_gateway->request_poll($request);
-
-		print_r($request);
-
-	}
-
-	if ($request['status'] === NULL) {
-		exit('Stopped waiting for a response after several attempts.');
-	}
 
 ?>
